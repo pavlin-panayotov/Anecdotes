4cc0fe4c9a32ff03efef1272e62860d304a2d32a
Pavlin Panayotov <panayotov1995@gmail.com>
2020-01-12 20:31:16 +0200

    Added initial loading screen.

--- Anecdotes/SceneDelegate.swift
+++ Anecdotes/SceneDelegate.swift
@@ -19,9 +19,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		guard let windowScene = (scene as? UIWindowScene) else { return }
 		
 		window = UIWindow(frame: UIScreen.main.bounds)
-		let vc = UIViewController()
-		vc.view.backgroundColor = .green
-		window?.rootViewController = vc
+		window?.rootViewController = LoadingViewController()
 		window?.makeKeyAndVisible()
 		window?.windowScene = windowScene
 	}
